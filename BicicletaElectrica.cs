64cf57543b8de1f8d47d7b80956616f0ecd3a682
Alvin Amaya <amayapinedaalvinasael@gmail.com>
2026-02-14 15:37:09 -0600

    Implemetar Taxi y Bicicleta

---
+++ BicicletaElectrica.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionTransporteUrbano
+{
+    internal class BicicletaElectrica: Vehiculo
+    {
+        private double costoEnergiaPorKm;
+        public BicicletaElectrica(string placa, int capacidad, double velocidadMaxima, double kilometrosRecorridos, double costoEnergiaPorKm) : 
+            base(placa, capacidad, velocidadMaxima, kilometrosRecorridos) 
+        {
+            this.costoEnergiaPorKm = costoEnergiaPorKm;
+        }
+        public override void calcularCostoViaje()
+        {
+            Console.WriteLine($"Costo del viaje: {costoEnergiaPorKm * this.KilometrosRecorridos}");
+        }
+        public override void mostrarInformacion()
+        {
+            Console.WriteLine($"Bicicleta Eléctrica - Placa: {this.Placa}" +
+                $"Capacidad: {this.Capacidad}" +
+                $"Velocidad Máxima: {this.VelocidadMaxima} km/h" +
+                $"Kilómetros Recorridos: {this.KilometrosRecorridos} km" +
+                $"Costo de energia por Km: {this.costoEnergiaPorKm}");
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionTransporteUrbano
+{
+    internal class Taxi: Vehiculo
+    {
+        private double tarifaBase;
+        private double tarifaPorKm;
+        public Taxi(string placa, int capacidad, double velocidadMaxima, double kilometrosRecorridos, double tarifaBase, double tarifaPorKm) : 
+            base(placa, capacidad, velocidadMaxima, kilometrosRecorridos) 
+        {
+            this.tarifaBase = tarifaBase;
+            this.tarifaPorKm = tarifaPorKm;
+        }
+        public override void calcularCostoViaje()
+        {
+            Console.WriteLine($"Costo del viaje: {tarifaBase + (tarifaPorKm * this.KilometrosRecorridos)}");
+        }
+        public override void mostrarInformacion()
+        {
+            Console.WriteLine($"Taxi - Placa: {this.Placa}" +
+                $"Capacidad: {this.Capacidad}" +
+                $"Velocidad Máxima: {this.VelocidadMaxima} km/h" +
+                $"Kilómetros Recorridos: {this.KilometrosRecorridos} km" +
+                $"Tarifa Base: {this.tarifaBase}" +
+                $"Tarifa por Km: {this.tarifaPorKm}");
+        }
+    }
+}
