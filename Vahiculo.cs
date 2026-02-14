94928242bf0f518ffe756c6d2c81e707fba2d8b2
Alvin Amaya <amayapinedaalvinasael@gmail.com>
2026-02-14 14:38:47 -0600

    Agregar archivos de proyecto.

---
+++ Vahiculo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionTransporteUrbano
+{
+    internal abstract class Vehiculo
+    {
+        private String placa;
+        private int capacidad;
+        private double velocidadMaxima; //  km/h
+        private double kilometrosRecorridos;
+
+        public Vehiculo(string placa, int capacidad, double velocidadMaxima, double kilometrosRecorridos)
+        {
+            this.placa = placa;
+            this.capacidad = capacidad;
+            this.velocidadMaxima = velocidadMaxima;
+            this.kilometrosRecorridos = kilometrosRecorridos;
+        }
+
+        public void recorrerKilometros(double kilometros)
+        {
+            kilometrosRecorridos += kilometros;
+        }
+
+        public abstract void calcularCostoViaje();
+        public abstract void mostrarInformacion();
+    }
+}
